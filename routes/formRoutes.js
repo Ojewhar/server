7aa24c85bca2237ad54d8405a77f7942d9a56249
dev.abusalem <dev.abusalem@gmail.com>
2024-01-08 21:07:40 +0600

    update enviro

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -4,16 +4,10 @@ const { authGuard } = require("../middlewares/tokenVerification");
 const {
   createFormOne,
   getFormOneData,
-  createConsultation,
-  getConsultation,
 } = require("../controllers/formController");
 
 // define routers
 router.post("/createFormOne", createFormOne);
 router.get("/getFormOne", authGuard, getFormOneData); //(its already check authGurd From Frontend)
-
-// UpDocFormConsultation Form Route
-// router.post("/createConsultation", createConsultation);
-// router.get("/getConsultation", tokenVerification, getConsultation);
 
 module.exports = router;
