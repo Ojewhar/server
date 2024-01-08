eaed592e2a6c8d6b23468342de1eec50dc0c2a55
dev.abusalem <dev.abusalem@gmail.com>
2024-01-08 23:44:59 +0600

    update cors

--- api/index.js
+++ api/index.js
@@ -31,15 +31,15 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 app.use((req, res, next) => {
   if (req.originalUrl.includes("/stripe/webhook")) {
     bodyParser.text({ type: "application/json" })(req, res, next);
