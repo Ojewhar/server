accdc6fcfee3fdfdf7f8594d7b1249fd19cdb9b6
dev.abusalem <dev.abusalem@gmail.com>
2024-01-09 17:24:01 +0600

    updfsfsd

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ dotenv.config();
 app.use(express.json());
 
 app.get("/", (res, req) => {
-  try {
-    res.status(200).json("App working fine");
-  } catch (error) {
-    res.status(400).json("Server error");
-  }
+  res.end("App working fine");
 });
 
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+app.post("/", (req, res) => {
+  res.end("App working fine");
+});
+
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
 });
